43cb7cc6deb75496f1a620cc15867f117c62f75c
Carver Goldstein <carverg@nevada.unr.edu>
2021-08-28 19:57:25 -0700

    Glossary has book

--- glossary.lua
+++ glossary.lua
@@ -9,17 +9,19 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ]]
 glossary = {}
-glossary.leftwriting = "Evolution: An overall change of a population's gene pool over time through processes such as mutation and natural selection.\nNatural Selection: A process that explains how organisms with traits and genetic characteristics that are beneficial to survive in their environment are passed down to future generations while those less adapted dwindle in population.\nTrait: A genetic characteristic among similar organisms in a species."
-glossary.rightwriting = "Species: The basic classification in biology that describes a group of organisms that resemble one another and are able to reproduce among themselves.\nVariation: In biology, a difference in a characteristic or trait from one organism in a species to another in the same species.\nReproduction: The process that enables organisms to generate new organisms to ensure continuation of the species.\nOffspring: The children of a parent; a descendant."
+glossary.leftwriting = "Evolution: The unifying theme of the life sciences. Evolution explains how all living things on this planet are related and how we all descend from common ancestors. It explains how this vast, beautiful array of millions and millions of species got here in the first place.\nNatural Selection: One of the driving mechanisms of evolution. Simply stated, natural selection means that random genetic mutations arise in a population of living things. Some of these mutations are harmful and will hurt the individual’s chances of surviving. Other mutations are neutral, they do not help or hurt the individual.  Sometimes, a mutation can be beneficial, helping the individual survive better than its companions. If this mutation is passed on to its offspring, the entire population can change over time, with more and more individuals exhibiting the desirable characteristic."
+glossary.rightwriting = "Trait: A physical characteristic found in a species, for example eye color or height. Traits are controlled by genetic factors.\nSpecies: Loosely stated, a group of living things who can reproduce together, producing offspring who can reproduce together. (A horse and a donkey can produce a mule, but a mule cannot reproduce. So horses and donkeys are considered two separate species.\nHypothesis: A testable statement.\nTheory: A broad explanation of a natural phenomena based on facts, repeatedly-tested hypotheses, and laws. The Theory of Gravitational Attraction explains why we have gravity. The Heliocentric Theory explains how the sun is the center of our Solar System.\nVariation: the different “flavors” of a trait. For example, eye color is the trait, brown, blue, green, and hazel eyes are the possible variations of that trait."
 function glossary.load()
 	love.graphics.setBackgroundColor(205/255, 133/255, 63/255)
 	glossary.background = love.graphics.newImage("images/WoodBackground.jpg")
+	glossary.book = love.graphics.newImage("images/Book.png")
 	glossary.hand = love.mouse.getSystemCursor("hand")
 	glossary.smallfont = love.graphics.newFont(20)
 	glossary.middlefont = love.graphics.newFont(25)
 end
 function glossary.draw()
 	love.graphics.draw(glossary.background, 0, 0)
+	love.graphics.draw(glossary.book, 0, 60)
 	love.graphics.setFont(glossary.smallfont)
 	love.graphics.setColor(139/255, 69/255, 19/255)
 	love.graphics.rectangle("fill", 0, 0, 170, 60, 10, 8)
@@ -37,12 +39,9 @@ function glossary.draw()
 	love.graphics.printf("Darwin's Bio", 683, 5, 171, "center")
 	love.graphics.setColor(0, 0, 0)
 	love.graphics.printf("Glossary", 854, 5, 171, "center")
-	love.graphics.setColor(1, 1, 1)
-	love.graphics.rectangle("fill", 50, 100, 924, 550)
-	love.graphics.setColor(0, 0, 0)
-	love.graphics.setFont(glossary.middlefont)
-	love.graphics.printf(glossary.leftwriting, 100, 100, 362, "left")
-	love.graphics.printf(glossary.rightwriting, 562, 100, 362, "right")
+	love.graphics.setFont(glossary.smallfont)
+	love.graphics.printf(glossary.leftwriting, 100, 85, 372, "left")
+	love.graphics.printf(glossary.rightwriting, 562, 85, 362, "right")
 	love.graphics.setColor(1, 1, 1)
 end
 function glossary.update(dt)
